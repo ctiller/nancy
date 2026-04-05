8fe2f8c264a1a6e16649cbf5d15f927103c09f74
Craig Tiller <craig.tiller@gmail.com>
2026-04-05 19:58:32 +0000

    chore: remove unused test_key.rs file

--- test_key.rs
+++
@@ -1,8 +0,0 @@
-use did_key::{Ed25519KeyPair, generate};
-use did_key::KeyMaterial;
-
-fn main() {
-    let key = generate::<Ed25519KeyPair>(None);
-    let sec = key.private_key_bytes();
-    let key2 = Ed25519KeyPair::from_secret_key(&sec);
-}
